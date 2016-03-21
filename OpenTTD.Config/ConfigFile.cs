c44afbb5996f6c54d3e8bcbeae9bbdffb9078c22
Matt Smith <matt@matt40k.uk>
2016-03-21 00:41:17 +0000

    Add config file location

---
+++ OpenTTD.Config/ConfigFile.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+
+namespace OpenTTD.Config
+{
+    public class ConfigFile
+    {
+        string _dir = null;
+
+        public ConfigFile()
+        {
+            SetDirectory = GetDefaultDirectory;
+        }
+
+        public ConfigFile(string dir)
+        {
+            if (Directory.Exists(dir))
+                _dir = dir;
+        }
+
+        public string GetFileName
+        {
+            get { return "openttd.cfg"; }
+        }
+
+        public string GetFilePath
+        {
+            get { return Path.Combine(GetDirectory, GetFileName; }
+        }
+
+        public string GetDirectory
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(_dir) && Directory.Exists(_dir))
+                    return _dir;
+            }
+        }
+
+        public string SetDirectory
+        {
+            set
+            {
+                if (Directory.Exists(value))
+                    _dir = value;
+            }
+        }
+
+        public string GetDefaultDirectory
+        {
+            get
+            {
+                string myDoc = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                if (Directory.Exists(myDoc))
+                {
+                    string configDir = Path.Combine(myDoc, "OpenTTD");
+                    if (!Directory.Exists(configDir))
+                    {
+                        // The folder doesn't exist yet, lets try create it
+                        try
+                        {
+                            Directory.CreateDirectory(configDir);
+                        }
+                        catch (IOException CreateConfigDir)
+                        {
+                            
+                        }
+                        catch (UnauthorizedAccessException DeniedCreateConfigDir)
+                        {
+
+                        }
+                    }
+                    if (Directory.Exists(configDir))
+                        return configDir;
+                }
+                return Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            }
+        }
+    }
+}
